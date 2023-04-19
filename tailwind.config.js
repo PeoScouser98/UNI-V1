b6b7405c77352d1c172c64201762403f6ec4f0e5
quanghiep03198 <quanghiep03198@gmail.com>
2023-04-19 18:43:38 +0700

    INIT

---
+++ tailwind.config.js
@@ -0,0 +1,36 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+	content: ["*.{html,js}"],
+	important: true,
+	mode: "jit",
+	theme: {
+		screens: {
+			xs: {
+				min: "300px",
+				max: "374px",
+			},
+			sm: {
+				min: "375px",
+				max: "767px",
+			},
+
+			md: {
+				min: "768px",
+				max: "1023px",
+			},
+			lg: {
+				min: "1024px",
+				max: "1365px",
+			},
+			xl: { min: "1366px" },
+			xxl: { min: "1920px" },
+		},
+		extend: {
+			fontFamily: {
+				sans: ["Inter var"],
+				clash: ["Clash Display", "sans-serif"],
+			},
+		},
+	},
+	plugins: [require("prettier-plugin-tailwindcss"), require("daisyui")],
+};
